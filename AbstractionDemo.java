2e530b052fc23b2d4d6aa6c9b8dd6a0fbcf3738d
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2022-01-29 22:50:07 -0600

    Update AbstractionDemo.java

--- AbstractionDemo.java
+++ AbstractionDemo.java
@@ -1,54 +1,77 @@
-/* Jessica is attempting to create 4 methods that will add, substract, multiply and divide 2 doubles.
-* Help Jessica fulfill her task using the program below. */
 package KidsKodingVideos;
 import java.util.*;
-interface A { 
-    public double sum(double x, double y);
-    public double difference(double x, double y);
-    public double product(double x, double y);
-    public double quotient(double x, double y);
+import java.io.*;
+@SuppressWarnings("unused")
+/* FRQ: 
+ * 
+ * Jessica is assigned to make 4 abstract methods that add, substract, multiply, and divide two doubles, rounding each value to 2 decimal places.
+ * Using abstraction, assist Jessica in the given task.
+ * 
+ * Input: 3.2, 4.1
+ * Output: 
+ * Sum: 7.30
+ * Difference: -0.90
+ * Product: 13.12
+ * Quotient: 0.78
+ * 
+ * Input: 10, 5
+ * Output:
+ * Sum: 15.00
+ * Difference: 5.00
+ * Product: 50.00
+ * Quotient: 2.00
+*/
+abstract class FourMethods { //4 abstract methods that add, sub, multi, divide. All should return doubles
+	protected abstract double sum(double x, double y);
+	protected abstract double difference(double x, double y);
+	protected abstract double product(double x, double y);
+	protected abstract double quotient(double x, double y);
 }
-class HelperClass implements A {
-    @Override //optional but best practices. Tells java a method is being overridden
-    public double sum(double x, double y) {
-        return x + y;
-    }
-    @Override 
-    public double difference(double x, double y) {
-        return y - x;
-    }
-    @Override
-    public double product(double x, double y) {
-        return x * y;
-    }
-    @Override
-    public double quotient(double x, double y) {
-        return y / x;
-    }
+class UsingTheFourMethods extends FourMethods { //override abstract methods
+	@Override //java annotation telling the compiler, a method is being overridden
+	protected double sum(double x, double y) {
+		return x + y;
+	}
+
+	@Override
+	protected double difference(double x, double y) {
+		return x - y;
+	}
+
+	@Override
+	protected double product(double x, double y) {
+		return x * y;
+	}
+
+	@Override
+	protected double quotient(double x, double y) {
+		return x / y;
+	}
 }
 public class AbstractionDemo {
-    public static void main(String[] args) throws InputMismatchException {
-        double x;
-        double y;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Type a number!");
-        try {
-            x = scan.nextDouble();
-        }
-        catch(Exception e) {
-            throw new InputMismatchException("Try Again! Be sure to input a whole or decimal number");
-        }
-        System.out.println("Type another number!");
-        try {
-            y = scan.nextDouble();
-        }
-        catch(Exception e) {
-            throw new InputMismatchException("Try Again! Be sure to input a whole or decimal number");
-        }
-        HelperClass h = new HelperClass();
-        System.out.println("Sum: " + h.sum(x, y));
-        System.out.println("Differece: " + h.difference(x, y));
-        System.out.println("Product: " + h.product(x, y));
-        System.out.println("Quotient: " + h.quotient(x, y));
-    }
+	private double x;
+	private double y;
+	public static void main(String... args) throws InputMismatchException {
+		AbstractionDemo a = new AbstractionDemo();
+		UsingTheFourMethods f = new UsingTheFourMethods();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Type a number");
+		try {
+			a.x = scan.nextDouble(); //storing x (user's first number)
+		}
+		catch(Exception e) {
+			System.out.println("Be sure to type any number (including decimals).");
+		}
+		System.out.println("Type another number");
+		try {
+			a.y = scan.nextDouble(); //storing y (user's second number)
+		}
+		catch(Exception e) {
+			System.out.println("Be sure to type any number (including decimals).");
+		}
+		System.out.printf("Sum: %.2f\n", f.sum(a.x, a.y));
+		System.out.printf("Difference: %.2f\n", f.difference(a.x, a.y));
+		System.out.printf("Product: %.2f\n", f.product(a.x, a.y));
+		System.out.printf("Quotient: %.2f\n", f.quotient(a.x, a.y));
+	}
 }
