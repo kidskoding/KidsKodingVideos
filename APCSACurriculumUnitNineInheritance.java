50e6972f3176afb8a120f421cccc7f5a8f480d53
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2022-02-21 10:04:38 -0600

    Update and rename APCSACurriculum.java to APCSACurriculumUnitNineInheritance.java

--- APCSACurriculumUnitNineInheritance.java
+++ APCSACurriculumUnitNineInheritance.java
@@ -9,7 +9,7 @@ class B extends A { //child (sub) class
 		System.out.println("Certified Lover Boy - Drake");
 	}
 }
-public class APCSACurriculum {
+public class APCSACurriculumUnitNineInheritance {
 	public static void main(String[] args) {
 		A b = new B(); //This is polymorphism. Since B inherits A you can all an instance of class (object) A and pass it into B's constructor.
 		//Since we are passing into B's constructor, the showAlbum() method from class B is getting called and printed to the terminal
